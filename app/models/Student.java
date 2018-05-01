80d13186e899532a3ee9471a46a0d778ac62c586
robuntu <robuntu@robuntu.ca>
2018-05-01 09:29:24 -0400

    Added variables

---
+++ app/models/Student.java
@@ -0,0 +1,14 @@
+package models;
+
+public class Student {
+
+    public Integer studentNumber;
+    public Integer OEN;
+    public String firstName;
+    public String lastName;
+    public Integer grade;
+    public String email;
+    public String sex;
+    public Payment[] payments;
+
+}
