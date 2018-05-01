39c3b7f6f89969f323cfda6ede828e964454be34
parisngow <36411103+parisngow@users.noreply.github.com>
2018-05-01 09:45:24 -0400

    Add toString method and update class to play framework

--- app/models/Student.java
+++ app/models/Student.java
@@ -1,12 +1,22 @@
 package models;
 
-public class Student {
-    public Integer studentNumber;
-    public Integer OEN;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Student extends Model {
+    
+    @Id
+    public int studentNumber;
+    public int oen;
     public String firstName;
     public String lastName;
-    public Integer grade;
+    public int grade;
     public String email;
     public String sex;
     public Payment[] payments;
+    
+    public String toString() {
+        return lastName + firstName;
+    }
 }
