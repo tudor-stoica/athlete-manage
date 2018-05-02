5f4546b88ed8342661cc8d93901790050be63e35
SMinich <36411013+SMinich@users.noreply.github.com>
2018-05-02 08:58:13 -0400

    Update Student.java

--- app/models/Student.java
+++ app/models/Student.java
@@ -3,6 +3,16 @@ package models;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+/**
+ * This program establishes the "Student" class for the athlete management app. The student class
+ * will establish important attributes for each student athlete and obtain the number of points they
+ * have accumlated based on what teams they have played on.
+ *
+ *
+ * @version 1.0
+ * @author Scrum Group 1 (Brandon, Simon, Nathan, Tudor, Paris)
+ */
+
 @Entity
 public class Student extends Model {
     
