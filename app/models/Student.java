d1bfda947e6de7168c9fb820e2ae8eb80bc2b691
SMinich <36411013+SMinich@users.noreply.github.com>
2018-05-02 09:02:49 -0400

    Update Student.java

--- app/models/Student.java
+++ app/models/Student.java
@@ -26,7 +26,27 @@ public class Student extends Model {
     public String sex;
     public Payment[] payments;
     
+    
+    /**
+     * Displays the last name and first name of a student
+     * @param lastName String value of the last name of a student
+     * @param firstName String value of the first name of a student
+     * @return String value of the students name in the format lastName,firstName
+     */
     public String toString() {
         return lastName + ", " + firstName;
     }
+    
+    /**
+     * Takes an integer value of school year and identifies the number of points a student accumulated in that year.
+     * @param schoolYear Integer value of the year a student played sports
+     * @return integer value of points accumulated during that year
+     */
+    
+    /**
+     * Totals the number of points a student accumulated throughout their high school career
+     * @return integer value of the total number of points scored by a particular student
+     */
+    
+        
 }
