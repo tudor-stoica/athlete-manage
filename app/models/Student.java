b1d7540c698fdf726c4c81ae0fb76760b3ff7d35
robuntu <robuntu@robuntu.ca>
2018-05-02 08:55:28 -0400

    progress?

---
+++ app/models/Student.java
@@ -0,0 +1,16 @@
+package models;
+
+public class Student {
+    public Integer studentNumber;
+    public Integer OEN;
+    public String firstName;
+    public String lastName;
+    public Integer grade;
+    public String email;
+    public String sex;
+    public Payment[] payments;
+
+    public Integer getPoints(Schoolyear Schoolyear){
+        
+    }
+}
