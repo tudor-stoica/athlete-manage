f226005734c5c5a855f067014e7ff4e905916ca7
tudor-stoica <31967471+tudor-stoica@users.noreply.github.com>
2018-05-16 09:51:54 -0400

    Update StudentModelTest.java

--- test/models/StudentModelTest.java
+++ test/models/StudentModelTest.java
@@ -1,15 +1,11 @@
 package models;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import play.test.WithApplication;
+import static org.junit.Assert.*;
+import static play.test.Helpers.*;
 
-import static junit.framework.TestCase.assertEquals;
-import static play.test.Helpers.fakeApplication;
-import static play.test.Helpers.inMemoryDatabase;
-import static play.test.Helpers.start;
-
-public class StudentModelTest extends WithApplication{
+public class StudentModelTest extends WithApplication {
 
     @Before
     public void setUp() {
@@ -17,10 +13,42 @@ public class StudentModelTest extends WithApplication{
     }
 
     @Test
-    public void testTest(){
-        int out = 4;
-        int in = 2*2;
-        assertEquals(out, in);
+    public void testStudentIdInfo(){
+        Student test = new Student("John", "Doe");
+        test.studentNumber = 1034012;
+        test.oen = 365726342;
+        test.email = "john.doe19@ycdsbk12.ca";
+        assertEquals(1034012, test.studentNumber);
+        assertEquals(365726342, test.oen);
+        assertEquals("john.doe19@ycdsbk12.ca", test.email);
+    }
+
+    @Test
+    public void testStudentBasicInfo(){
+        Student test = new Student("John", "Doe");
+        test.sex = "Male";
+        test.grade = 12;
+        assertEquals("John",test.firstName );
+        assertEquals("Doe", test.lastName);
+        assertEquals(12, test.grade);
+    }
+
+    @Test
+    public void testToString() {
+        Student test = new Student("John", "Doe");
+        assertEquals("Doe, John", test.toString());
+    }
+
+    @Test
+    public void dataBaseTest(){
+        new Student ("John", "Doe").save();
+        Student test = Student.find.query().where().eq("firstName", "John").findOne();
+        assertEquals("John", test.firstName);
+    }
+
+    @Test
+    public void testGetPoints(){
+        Team teams = new Team();
     }
 
 }
