61c9bca75609eda5f946628ccd98e2c6bedba0d0
Portia Chang <32988308+pchang1693@users.noreply.github.com>
2018-06-07 15:34:04 -0400

    Add getPoints tests to StudentModelTest (#13)
    * Add getPoints tests to StudentModelTest
    
    * Add test for getPoints across all school years to StudentModelTest

--- test/models/StudentModelTest.java
+++ test/models/StudentModelTest.java
@@ -1,8 +1,10 @@
 package models;
 
+import helpers.SchoolYear;
 import org.junit.*;
 import play.test.WithApplication;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -43,5 +45,66 @@ public class StudentModelTest extends WithApplication {
         assertEquals(11, student.getPoints().intValue());
     }
 
-    // TODO: test getPoints(LocalDateTime) : Integer
+    @Test
+    public void testGetPointsAcrossAllSchoolYears() {
+        Student student = new Student("John", "Doe");
+        Spot s1 = new Spot();
+        Spot s2 = new Spot();
+        s1.points = 5;
+        s2.points = 6;
+
+        Team t1 = new Team();
+        t1.schoolYear = SchoolYear.toSchoolYear(LocalDateTime.of(2000, 1, 1, 0, 0, 0));
+        s1.team = t1;
+
+        Team t2 = new Team();
+        t2.schoolYear = SchoolYear.toSchoolYear(LocalDateTime.of(2011, 1, 1, 0, 0, 0));
+        s2.team = t2;
+
+        student.spots.add(s1);
+        student.spots.add(s2);
+
+        assertEquals(11, student.getPoints().intValue());
+    }
+
+    @Test
+    public void testGetPointsWithSchoolYear() {
+        Student student = new Student("John", "Doe");
+        Spot s1 = new Spot();
+        Spot s2 = new Spot();
+        s1.points = 5;
+        s2.points = 6;
+
+        Team team = new Team();
+        team.schoolYear = SchoolYear.toSchoolYear(LocalDateTime.of(2000, 1, 1, 0, 0, 0));
+        s1.team = team;
+        s2.team = team;
+        student.spots.add(s1);
+        student.spots.add(s2);
+
+        assertEquals(11, student.getPoints(SchoolYear.toSchoolYear(LocalDateTime.of(2000, 1, 1, 0, 0, 0))).intValue());
+    }
+
+    @Test
+    public void testGetPointsWithDifferentSchoolYears() {
+        Student student = new Student("John", "Doe");
+        Spot s1 = new Spot();
+        Spot s2 = new Spot();
+        s1.points = 5;
+        s2.points = 6;
+
+        Team t1 = new Team();
+        t1.schoolYear = SchoolYear.toSchoolYear(LocalDateTime.of(2000, 1, 1, 0, 0, 0));
+        s1.team = t1;
+
+        Team t2 = new Team();
+        t2.schoolYear = SchoolYear.toSchoolYear(LocalDateTime.of(2011, 1, 1, 0, 0, 0));
+        s2.team = t2;
+
+        student.spots.add(s1);
+        student.spots.add(s2);
+
+        assertEquals(5, student.getPoints(SchoolYear.toSchoolYear(LocalDateTime.of(2000, 1, 1, 0, 0, 0))).intValue());
+    }
+
 }
